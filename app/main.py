481a66514a54f44fd75bc9beeffa7d97511070d2
LeonardoYaranga <ljyaranga@espe.edu.ec>
2024-08-03 20:05:17 -0500

    update and delete

--- app/main.py
+++ app/main.py
@@ -30,3 +30,17 @@ def read_post(post_id: int, db: Session = Depends(get_db)):
 def read_posts(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     posts = crud.get_posts(db, skip=skip, limit=limit)
     return posts
+
+@app.put("/posts/{post_id}", response_model=schemas.Post)
+def update_post(post_id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
+    db_post = crud.update_post(db, post_id, post)
+    if db_post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return db_post
+
+@app.delete("/posts/{post_id}", response_model=schemas.Post)
+def delete_post(post_id: int, db: Session = Depends(get_db)):
+    db_post = crud.delete_post(db, post_id)
+    if db_post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return db_post
